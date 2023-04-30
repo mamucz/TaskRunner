7ca46cb5e87fb583bd2be0142dda38a8f3d560e7
mamucz <petr.ondra@rtprocessing.com>
2023-04-30 12:07:17 +0200

    Documentation upgrade

--- TaskRunner/ISender.cs
+++ TaskRunner/ISender.cs
@@ -1,6 +1,14 @@
 namespace TaskRunner;
-
+/// <summary>
+/// Interface for sending request
+/// </summary>
+/// <typeparam name="T">Obejct to send</typeparam>
 public interface ISender<in T>
 {
+    /// <summary>
+    /// Send the object async
+    /// </summary>
+    /// <param name="item">object to send</param>
+    /// <returns>return task</returns>
     Task SendAsync(T item);
 }
